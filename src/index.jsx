52412ed1933289ce03753ced519e7fed35dbbf65
Junmo <yunedoopea610@gmail.com>
2019-01-21 22:27:04 +0900

    Add third party libraries
    -react-router-dom
    -styled-components
    -eslint
    lint is applied as airbnb

---
+++ src/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+import App from './App';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+      monospace;
+  }
+`;
+
+ReactDOM.render(
+  <BrowserRouter>
+    <GlobalStyle />
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
