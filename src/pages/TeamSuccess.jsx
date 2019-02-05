639270cde6bbc39b6760f20bfbc70e7df23ae29b
Junmo <yunedoopea610@gmail.com>
2019-02-05 19:06:57 +0900

    Start TeamSuccess page

---
+++ src/pages/TeamSuccess.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+import Button from '../components/Button';
+
+const TeamSuccessContainer = styled.div`
+  width: 1180px;
+  margin: 0 auto;
+`;
+
+export default function TeamSuccess() {
+  return (
+    <TeamSuccessContainer />
+  );
+}
